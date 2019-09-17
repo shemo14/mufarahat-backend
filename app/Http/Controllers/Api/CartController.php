d940a87e459537783706e1402044e36c5338e2ad
Mohamed Shams <mo7amed.shams3477@gmail.com>
2019-09-17 10:08:44 +0200

    pull

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,11 +5,42 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use JWTAuth;
 
 class CartController extends Controller
 {
-    public function cart(){
-    	//
+    public function cart(Request $request){
+		if (!$request->header('Authorization') && !$request['device_id'])
+			return returnResponse(null, 'plz, enter auth token or device id to set save', 400);
+
+		if ($request->header('Authorization')){  // if user auth...
+			$user         		= JWTAuth::parseToken()->authenticate();
+			$cart_items		= Cart::where('user_id', $user->id)->get();
+			$all_products 	= [];
+
+			foreach ($cart_items as $item) {
+				$all_products[] = [
+					'cart_id' 		=> $item->id,
+					'product_id' 	=> $item->product_id,
+					'name'    		=> $item->product->name,
+					'quantity' 		=> $item->quantity,
+					'image'    		=> url('images/products') . '/' . $item->product->images()->first()->name,
+					'category'  	=> $item->product->category->name,
+					'desc'  		=> $item->product->desc,
+					'price'  		=> $item->product->price - ( $item->product->price * $item->product->discount ) / 100 . ' ' . trans('apis.rs'),
+					'rate'   		=>  $item->product->rate()->avg('rate')
+				];
+			}
+
+		}else{  // if user guest
+			$cart  					= new Cart();
+			$cart->device_id 		= $request->device_id;
+			$cart->product_id 		= $request->product_id;
+			$cart->quantity 		= $request->quantity;
+			$cart->save();
+		}
+
+		return returnResponse(NULL, trans('apis.set_cart'), 200);
 	}
 
 	public function set_cart(Request $request){
