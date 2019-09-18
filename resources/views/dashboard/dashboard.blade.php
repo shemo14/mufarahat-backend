dd72cc7d14417afc5724b28bc612aff0ade4e1d2
gethub23 <aa926626@gmail.com>
2019-09-18 13:36:55 +0200

    homepage

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -35,6 +35,45 @@
         <div class="col-lg-4 col-xs-6">
             <div class="small-box smallBoxCustom bg-aqua">
                 <div class="inner">
+                    <h3>{{$roles}}</h3>
+                    <p> عدد الصلاحيات</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-lock"></i>
+                </div>
+                <a href="{{route('permissionslist')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$cities}}</h3>
+                    <p> عدد المدن</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <a href="{{route('cities')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$wharehouses}}</h3>
+                    <p> عدد المستودعات</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <a href="{{route('warehouses')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+        
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
                     <h3>{{$categories}}</h3>
                     <p> عدد الاقسام</p>
                 </div>
@@ -44,17 +83,95 @@
                 <a href="{{route('categories')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
             </div>
         </div>
+        
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$products}}</h3>
+                    <p> عدد المنتجات</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <a href="{{route('products')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
 
         <div class="col-lg-4 col-xs-6">
             <div class="small-box smallBoxCustom bg-aqua">
                 <div class="inner">
-                    <h3>{{$roles}}</h3>
-                    <p> عدد الصلاحيات</p>
+                    <h3>{{$offers}}</h3>
+                    <p> عدد العروض</p>
                 </div>
                 <div class="icon">
-                    <i class="fa fa-lock"></i>
+                    <i class="fa fa-file-text-o"></i>
                 </div>
-                <a href="{{route('permissionslist')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+                <a href="{{route('offers')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$boxes}}</h3>
+                    <p> عدد البوكسات</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-file-text-o"></i>
+                </div>
+                <a href="{{route('boxs')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$packagins}}</h3>
+                    <p> عدد انواع التغليف</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-file-text-o"></i>
+                </div>
+                <a href="{{route('packaging')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$coupons}}</h3>
+                    <p> عدد الكوبونات </p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-file-text-o"></i>
+                </div>
+                <a href="{{route('coupons')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$questions}}</h3>
+                    <p> عدد اسئله الاستبيان</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-file-text-o"></i>
+                </div>
+                <a href="{{route('questions')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-xs-6">
+            <div class="small-box smallBoxCustom bg-aqua">
+                <div class="inner">
+                    <h3>{{$commanQues}}</h3>
+                    <p> عدد ا الاسئله  الشائعه </p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-file-text-o"></i>
+                </div>
+                <a href="{{route('commonQus')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
             </div>
         </div>
 
@@ -70,6 +187,7 @@
                 <a href="{{route('allreports')}}" class="small-box-footer"> الذهاب <i class="fa fa-arrow-circle-left"></i></a>
             </div>
         </div>
+
     </div>
 
 @endsection
