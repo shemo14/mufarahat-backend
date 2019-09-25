d83b8b4dd77554dfc4da82e967641955510c412b
Mohamed Shams <mo7amed.shams3477@gmail.com>
2019-09-25 13:15:57 +0200

    notification helper apis

---
+++ resources/lang/ar/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title_offer_notification'      => 'عرض جديد',
+    'body_offer_notification'       => 'عرض جديد علي :product_name من قبل :user_name',
+    'title_refused_offer'           => 'رفض العرض',
+    'body_refused_offer'            => 'تم رفض عرضك علي :product_name',
+    'title_accepted_offer'          => 'قبول العرض',
+    'body_accepted_offer'           => 'تم قبول عرضك علي :product_name',
+    'title_comment_notification'    => 'تعليق جديد',
+    'body_comment_notification'     => 'علق :user_name علي :product_name',
+    'title_like_notification'       => 'اعجاب جديد',
+    'body_like_notification'        => 'تم الاعجاب علي :product_name من قبل :user_name',
+    'title_admin_notification'      => 'اشعار اداري',
+];
---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'title_offer_notification'      => 'new offer',
+    'body_offer_notification'       => 'new offer on :product_name from :user_name',
+    'title_refused_offer'           => 'offer refused',
+    'body_refused_offer'            => 'your offer on :product_name was refused',
+    'title_accepted_offer'          => 'offer accepted',
+    'body_accepted_offer'           => 'your offer on :product_name was accepted',
+    'title_comment_notification'    => 'new comment',
+    'body_comment_notification'     => 'new comment from :user_name on :product_name',
+    'title_like_notification'       => 'new like',
+    'body_like_notification'        => 'your product :product_name was liked by :user_name',
+    'title_admin_notification'      => 'admin notification',
+];
